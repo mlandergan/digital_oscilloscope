57e2a0f89db1b20be9b0d5ee2ae06fbaca6ee4e7
stoneil <seanogon@gmail.com>
2018-11-08 15:46:34 -0500

    fixed extern issue with ADC buffer

--- ADC_implementation.h
+++ ADC_implementation.h
@@ -24,7 +24,7 @@
 #define ADC_BUFFER_SIZE 2048                             // size must be a power of 2
 #define ADC_BUFFER_WRAP(i) ((i) & (ADC_BUFFER_SIZE - 1)) // index wrapping macro
 extern volatile int32_t gADCBufferIndex;  // latest sample index
-extern volatile uint16_t gADCBuffer;           // circular buffer
+extern volatile uint16_t gADCBuffer[ADC_BUFFER_SIZE];    // circular buffer
 volatile uint32_t gADCErrors;                       // number of missed ADC deadlines
 #define ADC_SAMPLING_RATE 1000000   // [samples/sec] desired ADC sampling rate
 #define CRYSTAL_FREQUENCY 25000000  // [Hz] crystal oscillator frequency used to calculate clock rates
